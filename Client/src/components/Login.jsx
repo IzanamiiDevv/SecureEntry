b61229566e3aa782938eee8c8509bf6a5871b790
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-18 00:27:46 +0800

    sstyle

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -6,6 +6,18 @@ function Login(props){
     <section className="form">
       <title>Login</title>
       <h1 id="titlebar">Login</h1>
+      <div className="container">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name"placeholder="IzanamiiDevv"/>
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" placeholder="1234567890"/>
+        <br />
+        <label htmlFor="hidepass">Show Password</label>
+        <input type="checkbox"id="hidepass" />
+      </div>
+      <button>Confirm</button>
+      <br />
       <button onClick={()=>{
         props.signup(false);
       }}>Signup</button>
--- Client/src/components/Signup.jsx
+++ Client/src/components/Signup.jsx
@@ -6,6 +6,18 @@ function Signup(props){
     <section className="form">
       <title>Signup</title>
       <h1 id="titlebar">Signup</h1>
+      <div className="container">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" placeholder="IzanamiiDevv"/>
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" placeholder="1234567890"/>
+        <br />
+        <label htmlFor="hidepass">Show Password</label>
+        <input type="checkbox"id="hidepass" />
+      </div>
+      <button>Confirm</button>
+      <br />
       <button onClick={()=>{
         props.login(true)
       }}>Login</button>
