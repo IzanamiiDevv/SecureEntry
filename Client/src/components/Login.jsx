5b02d3b7f9be0b453096477e02b39f70065f6a99
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-17 21:50:50 +0800

    jsx

---
+++ Client/src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Login(){
+  return (
+    <>
+      <title>Login</title>
+      <h1>Login</h1>
+    </>
+  )
+}
+
+export default Login;
---
+++ Client/src/components/Signup.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Signup(){
+  return (
+    <>
+      <title>Signup</title>
+      <h1>Signup</h1>
+    </>
+  )
+}
+
+export default Login;
