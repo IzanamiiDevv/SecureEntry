7d4f8f7a72686e14d0f5983288d56542d8c40a3e
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-20 21:01:11 +0800

    test

--- test.js
+++ test.js
@@ -1,2 +1,51 @@
-require('dotenv').config()
-console.log(process.env.DB_DATABASE);
+function removeLast(str){
+    if(str == '' || str.length == 1)return '';
+    let result = '';
+    for(let i = 0; i < str.length - 1; i++){
+        result += str[i]
+    }
+    return result;
+}
+
+
+function removeFirst(str){
+    if(str == '' || str.length == 1)return '';
+    let result = '';
+    for(let i = 1; i < str.length; i++){
+        result += str[i]
+    }
+    return result;
+}
+
+function removeChar(str,char){
+    if(str == '' || str.length == 1)return '';
+    let result = '';
+    for(let i = 0; i < str.length; i++){
+        if(str[i] == char){result += ''}else{result += str[i]}
+    }
+    return result;
+}
+
+function verticalPiramid(lenght){
+    if(lenght == 1){'*'};
+    let stars = '';
+    for(let i = 0; i < lenght; i++){
+        stars += '*';
+        console.log(stars);
+    }
+
+    for(let i = 1; i <= lenght - 1; i++){
+        let result = '';
+        for(let j = 0; j < stars.length - i; j++){
+            result += stars[i]
+        }
+        console.log(result);
+    }
+}
+
+const txt = "Hello World";
+console.log(removeFirst(txt));
+console.log(removeLast(txt));
+console.log(removeChar(txt,'l'));
+
+verticalPiramid(5);
